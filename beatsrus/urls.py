2e07840f699bd2f2b1e5c73676f87b208386eb6c
sakimastringer <sakimastringer@gmail.com>
2023-04-19 19:58:48 -0500

    Full Crud for Albums and started Auth

--- beatsrus/urls.py
+++ beatsrus/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for beatsrus project.
+"""URL configuration for beatsrus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # route for home 
   path('', views.home, name='home'),
+  # route for about
+  path('about/', views.about, name='about'),
+  # route for album index
+  path('album/', views.album_index, name='index'),
 ]
