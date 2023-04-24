a7854890648c7671991deb7eef4cdae55d8626e0
sakimastringer <sakimastringer@gmail.com>
2023-04-24 18:42:44 -0500

    Config deployment

--- beatsrus/settings.py
+++ beatsrus/settings.py
@@ -136,3 +136,8 @@ LOGIN_REDIRECT_URL = '/albums/'
 # Add this variable to specify where logging out redirects to
 LOGOUT_REDIRECT_URL = '/'
 
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
